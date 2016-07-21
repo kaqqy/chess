20cf4d6b66c352c3800bca2c95aeeebdbeace934
James <james.shi@sbcglobal.net>
2016-07-21 13:44:00 -0700

    version to revert to in case new changes dont work

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Chess = require('./chess.min').Chess;
 
+var openRooms = {};
+var gamesIP = {}; //in progress
+
 //test
-var chess = new Chess();
+/*var chess = new Chess();
 
 while (!chess.game_over()) {
 	var moves = chess.moves();
@@ -14,7 +17,7 @@ while (!chess.game_over()) {
 	console.log(chess.ascii());
 	console.log(move);
 	chess.move(move);
-}
+}*/
 //endtest
 
 app.get('/', function (req, res) {
@@ -22,6 +25,29 @@ app.get('/', function (req, res) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function (socket) {
+	//sends all available room information
+	socket.emit('updateRooms', openRooms);
+
+	socket.on('joinGame', function (roomName, color) {
+		socket.join(roomName);
+		if (io.sockets.adapter.rooms[roomName].length >= 3) {
+			socket.leave(roomName);
+			return;
+		}
+		if (io.sockets.adapter.rooms[roomName].length == 1) {
+			gamesIP[roomName] = {color: socket};
+			openRooms[roomName] = (color == 'black' ? 'white' : 'black');
+		}
+		else {
+			gamesIP[roomName][color] = socket;
+			delete openRooms[roomName];
+		}
+		io.emit('updateRooms', openRooms);
+		io.emit('updateGames', gamesIP);
+	});
+});
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 2000;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
